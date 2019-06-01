a98abbd692c6d4868315e6979107b64286bfe1d0
John Larsen <john@pcis.se>
2019-06-01 12:52:49 +0200

    #7 Rename project tests  and added some new tests

---
+++ src/FileRego/DeleteRule/DeleteTest.cs
@@ -0,0 +1,52 @@
+﻿using FileRego.Rules;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeleteRuleTests
+{
+    [TestClass]
+    public class DeleteTest
+    {
+        [TestMethod]
+        public void Whene_deleteText_is_used_correct_string_is_returned()
+        {
+            //Arrange
+            var fileName = "ThisIsafilename.txt";
+            var exspected = "Thisafilename.txt";
+
+            //Act
+            var result = fileName.DeleteText("Is");
+
+            //Assert
+            Assert.AreEqual(exspected, result);
+        }
+
+        [TestMethod]
+        public void When_Extension_is_true_and_there_is_no_extension_this_is_handled_correct()
+        {
+            //Arrange
+            var fileName = "ThiIsFilenamewithtxt_no_extension";
+            var exspected = "ThiIsFilenamewith_no_extension";
+
+            //Act
+            var result = fileName.DeleteText("txt", false, false);
+
+            //Assert
+            Assert.AreEqual(exspected,result);
+        }
+
+        [TestMethod]
+        public void When_Extension_is_deleted_this_is_handled_correct()
+        {
+            //Arrange
+            var fileName =  "ThiIsFilenamewithtxt.txt";
+            var exspected = "ThiIsFilenamewith.";
+
+            //Act
+            var result = fileName.DeleteText("txt", false, false);
+
+            //Assert
+            Assert.AreEqual(exspected, result);
+        }
+
+    }
+}
--- src/FileRego/DeleteRule/Properties/AssemblyInfo.cs
+++ src/FileRego/DeleteRule/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("DeleteRule")]
+[assembly: AssemblyTitle("DeleteRuleTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DeleteRule")]
+[assembly: AssemblyProduct("DeleteRuleTests")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
